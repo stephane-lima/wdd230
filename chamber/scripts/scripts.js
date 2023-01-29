9f74f92318543ce02edce5393f23a0f1886b5f0b
stephane-lima <ggabriellopess99999@gmail.com>
2023-01-29 03:07:57 -0100

    Add Homepage Images

---
+++ chamber/scripts/scripts.js
@@ -0,0 +1,40 @@
+const menuButton = document.querySelector('#menuButton');
+
+menuButton.addEventListener('click', function() {
+    toggleMenu();
+});
+
+
+function toggleMenu() {
+    const navElement = document.querySelector('#primaryNav');
+    navElement.classList.toggle('open');
+    document.querySelector('#menuButton').classList.toggle("open");
+}
+
+function getLastUpdate() {
+    let lastModified = new Date(document.lastModified).toLocaleDateString('en-US', {day: 'numeric', month: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'});
+    return lastModified;
+}
+
+function getCurrentYear() {
+    return new Date().getFullYear();
+}
+
+function today() { 
+    return new Date().toLocaleDateString('en-US', {dateStyle: 'full'});
+}
+
+let lastUpdatedElem1 = document.getElementById("lastmodifieddate1");
+lastUpdatedElem1.innerHTML = getLastUpdate();
+
+let lastUpdatedElem2 = document.getElementById("lastmodifieddate2");
+lastUpdatedElem2.innerHTML = getLastUpdate();
+
+let currentYearElem1 = document.getElementById('year1');
+currentYearElem1.innerHTML = getCurrentYear();
+
+let currentYearElem2 = document.getElementById('year2');
+currentYearElem2.innerHTML = getCurrentYear();
+
+let todaysDayElem = document.getElementById('date');
+todaysDayElem.innerHTML = today();
